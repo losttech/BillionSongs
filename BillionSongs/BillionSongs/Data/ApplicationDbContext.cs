f25f0d4f781c43d0ed765198cec59929ce9e2b8e
Victor Milovanov <lost@losttech.software>
2019-03-17 00:00:17 -0700

    replaced the default template with one, that has authentication

---
+++ BillionSongs/BillionSongs/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillionSongs.Data {
+    public class ApplicationDbContext : IdentityDbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {
+        }
+    }
+}
