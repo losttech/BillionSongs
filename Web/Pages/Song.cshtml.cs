c6d78900b73a7920d3c2ce3f203f55a5da4cc39e
Victor Milovanov <lost@losttech.software>
2019-04-15 16:23:57 -0700

    song page now supports fallback in case generation fails

--- Web/Pages/Song.cshtml.cs
+++ Web/Pages/Song.cshtml.cs
@@ -7,20 +7,50 @@
 
     using JetBrains.Annotations;
 
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
 
     public class SongModel : PageModel
     {
         readonly ISongDatabase songDatabase;
+        readonly IRandomSongProvider randomSongProvider;
 
         public Song Song { get; private set; }
 
-        public async Task OnGetAsync(uint id, CancellationToken cancellation) {
-            this.Song = await this.songDatabase.GetSong(id, cancellation).ConfigureAwait(false);
+        public async Task<IActionResult> OnGetAsync(uint id, string fallback = null, CancellationToken cancellation = default) {
+            if (!this.ModelState.IsValid)
+                return this.BadRequest();
+            try {
+                this.Song = await this.songDatabase.GetSong(id, cancellation).ConfigureAwait(false);
+                if (this.Song.GeneratorError != null) {
+                    switch (fallback) {
+                    case "random":
+                        uint randomID = await this.randomSongProvider.GetRandomSongID(cancellation).ConfigureAwait(false);
+                        return this.RedirectToPage("Song", routeValues: new {
+                            id = randomID, fallback = "random",
+                        });
+                    default:
+                        return this.NotFound();
+                    }
+                }
+                return new PageResult();
+            }
+            catch (LyricsGeneratorException) {
+                switch (fallback) {
+                case "random":
+                    uint randomID = await this.randomSongProvider.GetRandomSongID(cancellation).ConfigureAwait(false);
+                    return this.RedirectToPage("Song", routeValues: new {
+                        id = randomID, fallback = "random",
+                    });
+                default:
+                    return this.NotFound();
+                }
+            }
         }
 
-        public SongModel([NotNull] ISongDatabase songDatabase) {
+        public SongModel([NotNull] ISongDatabase songDatabase, [NotNull] IRandomSongProvider randomSongProvider) {
             this.songDatabase = songDatabase ?? throw new ArgumentNullException(nameof(songDatabase));
+            this.randomSongProvider = randomSongProvider ?? throw new ArgumentNullException(nameof(randomSongProvider));
         }
     }
 }
