e7e41955f22d63e82896c2c1c9bd30d028682858
Victor Milovanov <lost@losttech.software>
2019-04-15 16:20:28 -0700

    DummyLyricsGenerator now fails for songs with even ID (enables testing)

--- Web/DummyLyricsGenerator.cs
+++ Web/DummyLyricsGenerator.cs
@@ -4,7 +4,8 @@
     using System.Threading.Tasks;
     public class DummyLyrics : ILyricsGenerator {
         public async Task<string> GenerateLyrics(uint song, CancellationToken cancellation) {
-            await Task.Delay(TimeSpan.FromSeconds(5)).ConfigureAwait(false);
+            if ((song & 1) == 0) throw new LyricsGeneratorException("Dummy only generates odd songs");
+            await Task.Delay(TimeSpan.FromSeconds(5), cancellation).ConfigureAwait(false);
             return song.ToString();
         }
     }
