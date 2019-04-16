08e19a3fd7159c1b8a40d213d34dc0abcd0d1df4
Victor Milovanov <lost@losttech.software>
2019-04-15 21:12:28 -0700

    enabled memory cache limiting

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -33,7 +33,9 @@ public class Startup {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMemoryCache();
+            services.AddMemoryCache(options => {
+                options.SizeLimit = this.Configuration.GetValue<long?>("MemCache:SizeLimit", null);
+            });
             
             ILyricsGenerator lyricsGenerator =
                 this.Configuration.GetValue<string>("Generator", null) == "dummy"
