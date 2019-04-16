f5c5d71ac36239a66da2ef228de887302d5e6293
Victor Milovanov <lost@losttech.software>
2019-04-15 20:54:50 -0700

    explicitly enable memory cache in ConfigureServices

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -33,6 +33,8 @@ public class Startup {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            services.AddMemoryCache();
+            
             ILyricsGenerator lyricsGenerator =
                 this.Configuration.GetValue<string>("Generator", null) == "dummy"
                 ? new DummyLyrics()
