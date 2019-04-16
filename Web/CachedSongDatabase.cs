e24b79d67119b0901d8cc03355f0ed52b3c6f75d
Victor Milovanov <lost@losttech.software>
2019-04-15 19:37:28 -0700

    song cache: approximate song cache usage

--- Web/CachedSongDatabase.cs
+++ Web/CachedSongDatabase.cs
@@ -38,15 +38,23 @@
             }
         }
 
-        Task<Song> GenerateCacheEntry(ICacheEntry cacheEntry, uint song, CancellationToken cancellation) {
+        Task<Song> GenerateCacheEntry(ICacheEntry cacheEntry, uint songID, CancellationToken cancellation) {
             lock (cacheEntry) {
                 if (cacheEntry.Value is Task<Song> generating) {
                     return generating;
                 } else {
                     cacheEntry.SetSlidingExpiration(TimeSpan.FromHours(24));
-                    Task<Song> result = this.FetchOrGenerateSong(song, cancellation);
+                    Task<Song> result = this.FetchOrGenerateSong(songID, cancellation);
                     cacheEntry.SetAbsoluteExpiration(TimeSpan.FromDays(7));
                     cacheEntry.Value = result;
+                    result.ContinueWith(songTask => {
+                        if (!songTask.IsCompletedSuccessfully) return;
+                        
+                        Song song = songTask.Result;
+                        cacheEntry.SetSize(
+                            song.Title?.Length + song.Lyrics?.Length +
+                            song.GeneratorError?.Length ?? 64);
+                    });
                     return result;
                 }
             }
