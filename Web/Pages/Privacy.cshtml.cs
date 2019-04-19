d3f833f8ffb45c1f2ad0c93a2342b2f7a93750e3
Victor Milovanov <lost@losttech.software>
2019-04-18 18:35:49 -0700

    enabled caching for Privacy page

--- Web/Pages/Privacy.cshtml.cs
+++ Web/Pages/Privacy.cshtml.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace BillionSongs.Pages {
+﻿namespace BillionSongs.Pages {
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    [ResponseCache(VaryByHeader = "User-Agent", Duration = 24 * 60 * 60)]
     public class PrivacyModel : PageModel {
         public void OnGet() {
         }
