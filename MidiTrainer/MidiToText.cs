f2f3823528b4cc6217bd38791beef084825f475d
Victor Milovanov <lost@losttech.software>
2019-07-01 20:19:31 -0700

    output lyrics with relative time codes

--- MidiTrainer/MidiToText.cs
+++ MidiTrainer/MidiToText.cs
@@ -1,9 +1,13 @@
 ﻿namespace BillionSongs.MidiTrainer {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.IO;
     using System.Text;
     using ManyConsole.CommandLineUtils;
+    using MidiSharp;
+    using MidiSharp.Events.Meta.Text;
+
     class MidiToText : ConsoleCommand {
         public override int Run(string[] remainingArguments) {
             if (remainingArguments.Length == 0) {
@@ -19,6 +23,27 @@
 
         void Convert(string filePath, string target) {
             Console.WriteLine($"{filePath} -> {target}");
+            using(var input = File.OpenRead(filePath)) {
+                var midi = MidiSequence.Open(input);
+                string[] lyrics = midi.SelectMany(track => track.Events)
+                    .OfType<BaseTextMetaMidiEvent>()
+                    .GroupBy(evt => evt.GetType())
+                    .Select(e => e.Key.Name + "\n" + string.Join(" ",
+                        e.Select(evt => $"{evt.Text.Trim(' ', '\t')}@{evt.DeltaTime}")))
+                    .ToArray();
+
+                if (lyrics.Length == 0)
+                    Console.WriteLine("(no lyrics found)");
+                else
+                    foreach (var kind in lyrics)
+                        Console.WriteLine(kind);
+
+                Console.WriteLine();
+                var list = midi.Tracks.SelectMany(track => track.Events).Select(evt => evt.GetType()).Distinct();
+                foreach (var entry in list) {
+                    Console.WriteLine(entry.ToString());
+                }
+            }
         }
 
         public MidiToText() {
