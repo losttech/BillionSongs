3bb90ff6ae2372d721ab9b1339536899d9009c64
Victor Milovanov <lost@losttech.software>
2020-09-10 00:10:54 -0700

    when using dummy generator, output hint how to switch to the real one

--- Web/DummyLyricsGenerator.cs
+++ Web/DummyLyricsGenerator.cs
@@ -6,7 +6,7 @@
         public async Task<string> GenerateLyrics(uint song, CancellationToken cancellation) {
             if ((song & 1) == 0) throw new LyricsGeneratorException("Dummy only generates odd songs");
             await Task.Delay(TimeSpan.FromSeconds(5), cancellation).ConfigureAwait(false);
-            return song.ToString();
+            return song.ToString() + " by DummyLyrics.\n\nRemove 'Generator': 'dummy' from settings to switch to the real one.";
         }
     }
 }
